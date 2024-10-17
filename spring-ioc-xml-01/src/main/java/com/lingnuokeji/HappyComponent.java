53d2d43f155a6a97e20e58829d3fd746b7eeeb66
Suxiaoxiang <2951096305@qq.com>
2024-10-17 17:19:04 +0800

    smm-spring

---
+++ spring-ioc-xml-01/src/main/java/com/lingnuokeji/HappyComponent.java
@@ -0,0 +1,13 @@
+package com.lingnuokeji;
+
+/**
+ * @UserName 程序员_Suxiaoxiang
+ * @date 2024/10/17 17:03
+ * @Version 1.0
+ */
+public class HappyComponent {
+    // 默认包含无参数构造函数
+    public void doWork() {
+        System.out.println("HappyComponent is working");
+    }
+}
